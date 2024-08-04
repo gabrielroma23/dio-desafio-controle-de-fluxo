8e74dd13a8f0b66272d91713a25cc3c1e46a5de9
Gabriel <gabrielpc23015@gmail.com>
2024-08-04 14:37:08 -0300

    commit inicial

--- src/Contador.java
+++ src/Contador.java
@@ -26,7 +26,7 @@ public class Contador {
             throw new ParametrosInvalidosException();
         }
         int contagem = parametroDois - parametroUm;
-        //realizar o for para imprimir os números com base na variável contagem
+        //realizar o for para imprimir os números com base na variável contagem.
         for (int i = 1; i <= contagem; i++) {
             System.out.println("Imprimindo o número: " + i);
         }
